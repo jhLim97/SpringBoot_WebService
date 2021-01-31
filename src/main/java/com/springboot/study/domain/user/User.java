c51dd091853a093b153d7dec8ad9c056a1f0c1e6
jhLim97 <wnsgur2749@naver.com>
2021-02-01 01:53:50 +0900

    구글 로그인 연동을 위한 User 클래스 생성

---
+++ src/main/java/com/springboot/study/domain/user/User.java
@@ -0,0 +1,49 @@
+package com.springboot.study.domain.user;
+
+import com.springboot.study.domain.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column
+    private String picture;
+
+    @Enumerated(EnumType.STRING) // JPA 로 데이터베이스 저장 시 Enum 값을 어떤 형태로 저장할지 결정(기본은 int 지만 데이터베이스에서 확인 시 의미가 불명확 해 여기서는 문자열로 지정)
+    @Column(nullable = false)
+    private Role role;
+
+    @Builder
+    public User(String name, String email, String picture, Role role) {
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    public User update(String name, String picture) {
+        this.name = name;
+        this.picture = picture;
+
+        return this;
+    }
+
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
+}
---
+++ src/main/java/com/springboot/study/domain/user/UserRepository.java
@@ -0,0 +1,9 @@
+package com.springboot.study.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email); // 소셜 로그인으로 반환되는 값 중 email 을 통해 이미 생성된 사용자인지 처음인지 판단하는 메서드
+}
